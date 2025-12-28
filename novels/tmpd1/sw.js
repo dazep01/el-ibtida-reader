b81ba361645f6b34a187d01c285c1343ef7399ab
dazep01 <cs.dazep@gmail.com>
2025-12-28 17:03:03 +0700

    Create sw.js

---
+++ novels/tmpd1/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'el-ibtida-tmpd1-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdn.tailwindcss.com', // Optional: cache CDN if desired
+  'https://unpkg.com/@phosphor-icons/web'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
